2c23838e9cf6e7dea57b66427adbcae9f960b4ca
gal-samuel-israel <webmaster@agency.co.il>
2023-02-26 17:55:36 +0200

    init

---
+++ javascripts/discourse/api-initializers/init-trace-2-portal.js
@@ -0,0 +1,49 @@
+import { apiInitializer } from "discourse/lib/api";
+
+export default apiInitializer("0.8", (api) => {
+
+  var blockModal;  
+
+  if (api.getCurrentUser()) {
+    const currentUser = api.getCurrentUser()
+
+    var debug = currentUser.admin && settings.enable_debug_for_admins;
+    var debugForUsers = settings.enable_debug_for_user_ids;
+    var debugForIDs = (debugForUsers) ? debugForUsers.split("|") : null;
+    if (debugForIDs && debugForIDs.includes(currentUser.id.toString())) {
+      debug = true;
+    }
+
+    var debug4All = settings.enable_debug_for_all;
+    if(debug4All){ debug = true; }
+    
+    //const user = container.lookup("service:current-user");
+
+    if(debug){          
+      console.log('trace-2-portal initializer:');
+      //console.log(user);
+      //console.log(currentUser.user_option);
+      console.log('admin: ' + currentUser.admin); 
+      console.log('id: ' + currentUser.id); 
+    }
+
+    var showOnlyToAdmins = settings.enable_modal_only_for_admins; //make this false to enable component all users
+    var isAdmin = (currentUser.admin)        
+    blockModal = (showOnlyToAdmins && !isAdmin);
+
+    if(!blockModal){
+      api.registerConnectorClass("above-site-header", "home-modal", {
+        shouldRender() {
+          return true;
+        },
+      });
+    
+      api.createWidget("home-modal-widget", {
+        tagName: "div.home-modal",
+      });
+    }
+
+  }  
+
+});
+
