318b612522da975f55c698e62de33863ac79cdf8
gal-samuel-israel <webmaster@agency.co.il>
2023-03-01 14:37:45 +0200

    add startPageTracking

--- javascripts/discourse/api-initializers/init-trace-2-portal.js
+++ javascripts/discourse/api-initializers/init-trace-2-portal.js
@@ -1,4 +1,5 @@
 import { apiInitializer } from "discourse/lib/api";
+import { startPageTracking } from 'discourse/lib/page-tracker';
 import { viewTrackingRequired } from 'discourse/lib/ajax';
 
 export default apiInitializer("0.8", (api) => {
