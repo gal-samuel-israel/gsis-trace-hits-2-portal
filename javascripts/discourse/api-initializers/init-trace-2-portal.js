f2bc96cd2fb78cf39d4fe60dbf27197aea3e6d73
gal-samuel-israel <webmaster@agency.co.il>
2024-04-15 18:14:12 +0300

    traverse searchMenu.search.activeGlobalSearchTerm

--- javascripts/discourse/api-initializers/init-trace-2-portal.js
+++ javascripts/discourse/api-initializers/init-trace-2-portal.js
@@ -147,7 +147,7 @@ export default apiInitializer("1.6", (api) => {
                 /* Add a callback for onKeyDown in search menu */
                 api.addSearchMenuOnKeyDownCallback((searchMenu, event) => {
                     console.log('onKeyDownCallback event', event);
-                    console.log('searchMenu.search: ', searchMenu.search);
+                    console.log('searchMenu.search.activeGlobalSearchTerm: ', searchMenu.search.activeGlobalSearchTerm);
                     if (event.key === "Enter") {
                         const traceTerm = document.getElementById("search-term").value;
                         if(debugSearchTracer){
